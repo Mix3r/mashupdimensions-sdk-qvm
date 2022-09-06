88a5efd767ba6fff73f62a91120424880fbf431d
Mix3r_Durachok <verysoft@yandex.ru>
2022-09-06 17:27:38 +0500

    amok bot character added for special use
    amok bot character added for special use

---
+++ data_to_include_upak0/botfiles/bots/amok_i.c
@@ -0,0 +1,45 @@
+//===========================================================================
+//	Amok	version 0.0
+//
+//
+//	Made with doktor's BotStudio v. 0.98
+//	http://www.planetquake.com/botstudio  e-mail: doktor@cutey.com
+//===========================================================================
+
+
+#include "inv.h"
+
+#define FS_HEALTH				4
+#define FS_ARMOR				4
+
+#define W_SHOTGUN				50
+#define W_MACHINEGUN			70
+#define W_GRENADELAUNCHER		40
+#define W_ROCKETLAUNCHER		120
+#define W_RAILGUN				85
+#define W_BFG10K				30
+#define W_LIGHTNING				50
+#define W_PLASMAGUN				50
+
+#define GWW_SHOTGUN				400
+#define GWW_MACHINEGUN			0.0
+#define GWW_GRENADELAUNCHER		400
+#define GWW_ROCKETLAUNCHER		400
+#define GWW_RAILGUN				400
+#define GWW_BFG10K				400
+#define GWW_LIGHTNING			400
+#define GWW_PLASMAGUN			400
+
+#define W_TELEPORTER			400
+#define W_MEDKIT				400
+#define W_QUAD					400
+#define W_ENVIRO				400
+#define W_HASTE					400
+#define W_INVISIBILITY			400
+#define W_REGEN					400
+#define W_FLIGHT				400
+
+#define FLAG_WEIGHT				400
+
+//
+#include "fw_items.c"
